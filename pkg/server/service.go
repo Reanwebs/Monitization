b4a5d8b6d868e5bf2511bd894442226849a90910
edwinsiby <edwinsibyrajakumary@gmail.com>
2023-09-11 17:05:35 +0530

    reward adding - grpc call to auth service added

--- pkg/server/service.go
+++ pkg/server/service.go
@@ -3,18 +3,23 @@ package server
 import (
 	"context"
 	"log"
+	"monit/pb/client/auth"
 	pb "monit/pb/server"
-	client "monit/pkg/client/conference"
+	authClient "monit/pkg/client/auth"
+	confClient "monit/pkg/client/conference"
+	"monit/pkg/common/utils"
 )
 
 type monitizationServer struct {
 	pb.UnimplementedMonitizationServer
-	confClient client.ConferenceClient
+	confClient confClient.ConferenceClient
+	authClinet authClient.AuthClient
 }
 
-func NewMonitizationServer(confClient client.ConferenceClient) *monitizationServer {
+func NewMonitizationServer(confClient confClient.ConferenceClient, authClient authClient.AuthClient) *monitizationServer {
 	return &monitizationServer{
 		confClient: confClient,
+		authClinet: authClient,
 	}
 }
 
@@ -23,6 +28,24 @@ func (m *monitizationServer) HealthCheck(ctx context.Context, req *pb.Request) (
 	result := "Monitization server running"
 	return &pb.Response{Result: result}, nil
 
+}
+
+func (m *monitizationServer) ParticipationReward(ctx context.Context, req *pb.ParticipationRewardRequest) (*pb.ParticipationRewardResponse, error) {
+	coins, err := utils.CoinReward(req.Minutes, req.ConferenceType)
+	if err != nil {
+		return nil, err
+	}
+
+	authResp, err := m.authClinet.AddCoins(ctx, &auth.AddCoinsRequest{UserID: req.UserID, Coins: coins})
+	if err != nil {
+		return nil, err
+	}
+
+	resp := &pb.ParticipationRewardResponse{
+		Result:    "Reward added succesfuly" + authResp.Result,
+		CoinCount: coins,
+	}
+	return resp, nil
 }
 
 func (m *monitizationServer) UserRewardHistory(ctx context.Context, req *pb.UserRewardHistoryRequest) (*pb.UserRewardHistoryResponse, error) {
